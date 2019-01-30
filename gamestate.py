7c148f6e56d04abe6d8b5b14829e47969bb99a22
SaviorOfSanity <35996469+SaviorOfSanity@users.noreply.github.com>
2019-01-30 04:54:50 -0500

    Added Basic Pokemon functionality
    Created dictionaries with the card info. Then used those dictionaries to search for basic pokemon. Checks to see if hand has any basic pokemon if the player's hand does then I set out the prize cards.

--- gamestate.py
+++ gamestate.py
@@ -1,3 +1,17 @@
+######################################################
+##                 Project Vulpix                   ##
+##                Senior Project 2                  ##
+##                 Andrew Siddall                   ##
+##                 Chris Crisson                    ##
+##               Matthew Bedillion                  ##
+##               Adlene Bellaoucha                  ##
+##               January 30, 2019                   ##
+######################################################
+
+###################DESCRIPTION########################
+## This is the game state file that handles all the game actions. It will serve as a kind of game engine for the Ai to use.
+## 
+
 class Gameboard():
     #Player Lists
     playerDeck = []
@@ -19,7 +33,9 @@ class Gameboard():
     playerDeckIndex = 0
     oppDeckIndex = 0
 
-
+    ## All of the player/opp member functions could possibly be combined into one function each and have a flag based on turn or access.
+    ## Just a thought to reduce redundant code. Currently, I am just trying to get code down, but if we choose to do this we can edit it in Phase 3.
+    ## This will also depend on how we handle turns and stuff like that. We can discuss it during our next weekly team meeting.
 
     def playerDrawCard(self):
         
@@ -31,13 +47,12 @@ class Gameboard():
         self.oppHand.append(self.oppDeck[self.oppDeckIndex])
         self.oppDeckIndex += 1
 
-    def playerIsBasic(self):
+    def playerIsBasic(self, i):
         #Searches player's hand for a basic pokemon
-        for i in range(self.playerHand.length()-1):  #loop length of player hand - 1
-            if playerHand[i] == 'Basic': #This will need to be changed once the card structure is finalized
-                return True
-            else:
-                return False
+        if self.playerHand[i]["Basic"] == True: #This will need to be changed once the card structure is finalized. Currently each card is a dict and the hand is a list of card dicts
+            return True
+        else:
+            return False
 
     def playerSetPrize(self):
         self.playerPrize.append(self.playerDeck[self.playerDeckIndex])
@@ -53,25 +68,64 @@ class Gameboard():
         # If Basics in hand then place a basic on bench and fill up the prize list
         # If no basics then set mulligan and shuffle/redraw
         i = 0
+        basic = False
 
         #Draw cards from player hand
         for i in range(7): # should do this 7 times...need to test to be sure  
             self.playerDrawCard()
             self.oppDrawCard()
-        print(self.playerHand)
-        print(self.oppHand)
+        #print(self.playerHand)
+        #print(self.oppHand)
 
         ###CHECK FOR BASIC HERE!!!  ##
+        for i in range(len(self.playerHand)-1):  #loop length of player hand - 1
+            if self.playerIsBasic(i):
+                basic = True #if atleast one basic is found
+                print("Basic found!!")
+            else:
+                print("No basic found!!")
+            
 
-        #If player has basic#
-        for i in range(6):
-            self.playerSetPrize()
-        print(self.playerPrize)
+        #If player has basic set up prices#
+        if basic: 
+            for i in range(6):
+                self.playerSetPrize()
+                print(self.playerPrize[i]["Name"])
 
     
         
-            
+card1 = {"Name" : "Zorua",
+         "Type" : "Pokemon",
+         "Basic" : True,
+         "Hp" : 60,
+         "Attack 1" : 20,
+         "Attack 2" : 50}
+card2 = {"Name" : "Ultra Ball",
+         "Type" : "Item",
+         "Basic" : False,
+         "Hp" : False,
+         "Attack 1" : "None", #Handle effects here in attacks
+         "Attack 2" : "None"}
+card3 = {"Name" : "Zoroark",
+         "Type" : "Pokemon",
+         "Basic" : False,
+         "Hp" : 100,
+         "Attack 1" : 50,
+         "Attack 2" : 100}
+card4 = {"Name" : "Guzma",
+         "Type" : "Supporter",
+         "Basic" : False,
+         "Hp" : False,
+         "Attack 1" : "None", #Handle effects here in attacks
+         "Attack 2" : "None"}
+card5 = {"Name" : "Dark Energy",
+         "Type" : "Energy",
+         "Basic" : False,
+         "Hp" : False,
+         "Attack 1" : "None", #Handle effects here in attacks
+         "Attack 2" : "None"}
+         
 obb = Gameboard()
-obb.playerDeck = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9', 'card01', 'card02', 'card03', 'card04', 'card05', 'card06']
-obb.oppDeck = ['card11', 'card12', 'card13', 'card14', 'card15', 'card16', 'card17']
+obb.playerDeck = [card1, card2, card3, card4, card5, card1, card2, card3, card2 , card1, card2 , card3, card1, card4, card3, card5, card5, card5, card5, card5,]
+obb.oppDeck = [card1, card2, card3, card2 , card1, card2 , card3, card1, card4, card3, card5]
 obb.setup()

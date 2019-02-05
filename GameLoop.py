ed71e2274c3af344b793dc55b6d692273eb3a740
SaviorOfSanity <35996469+SaviorOfSanity@users.noreply.github.com>
2019-02-05 14:56:57 -0500

    Added a deck and fixed some errors
    I added a deck, called Steel Sun, and fixed some errors in the Class Card in the GameManager file. I have an error with the two attack damages that will need to be addressed. It was some kind of Key error. I also realized that when Chris reorganized everything he left out the functions in the gamestate file like the randomizing decks, the setup functions, and that stuff. I will work on getting them ported over to Chris' new GameManager class, or where ever they need to go now.

--- GameLoop.py
+++ GameLoop.py
@@ -15,14 +15,381 @@ import SetListTest
 import GameManager
 from setlists import SUM
 
-class GameLoop():
-	## Initialize the gameboard
-	gameboard = GameManager.Gameboard()
-	
-	## Populate player deck
-	for i in range(60):
-		obj = GameManager.Card(SUM.SUM001)
-		gameboard.playerDeck.append(obj)
+def GameLoop():
+        ## Initialize the gameboard
+        gameboard = GameManager.Gameboard()
 
-	for i in gameboard.playerDeck:
-		print(i.Name)
+        ## Populate player deck
+        print("Starting player deck Population...")
+        obj = GameManager.Card(card1)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card1)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card2)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card2)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card3)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card3)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card3)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card4)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card4)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card5)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card5)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card6)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card7)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card7)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card8)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card9)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card10)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card10)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card11)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card11)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card12)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card13)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card13)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card14)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card15)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card15)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card16)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card17)
+        gameboard.playerDeck.append(obj)
+
+        for i in range(3):
+                obj = GameManager.Card(card18)
+                gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card19)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card19)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card20)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card20)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card21)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card22)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card22)
+        gameboard.playerDeck.append(obj)
+
+        obj = GameManager.Card(card23)
+        gameboard.playerDeck.append(obj)
+        obj = GameManager.Card(card23)
+        gameboard.playerDeck.append(obj)
+
+        for i in range(12):
+                obj = GameManager.Card(card24)
+                gameboard.playerDeck.append(obj)
+        for i in range(8):
+                obj = GameManager.Card(card25)
+                gameboard.playerDeck.append(obj)
+
+        print("Player deck created")
+
+        for i in gameboard.playerDeck:
+                print(i.Name)
+
+card1 = {'Name' : 'Solgaleo',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 2,
+        'Hp' : 160,
+        'Power' : False,
+        'Attacl1Damage' : 0,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'MC',
+        'Attacl2Damage' : 170,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'MMC',
+        'RetreatCost' : 3,
+        'Weakness' : 'R',
+        'Resistance' : 'P'}
+
+
+
+card2 = {'Name' : 'Cosmoem',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 1,
+        'Hp' : 90,
+        'Power' : False,
+        'Attacl1Damage' : 0,
+        'Attack1Effect' : 'Switch this Pokémon with 1 of your Benched Pokémon',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : 'None',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 3,
+        'Weakness' : 'P',
+        'Resistance' : ''}
+
+card3 = {'Name' : 'Cosmog',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 60,
+        'Power' : False,
+        'Attack1Damage' : 0,
+        'Attack1Effect' : 'Draw a card',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : 'None',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 1,
+        'Weakness' : 'P',
+        'Resistance' : ''}
+
+card4 = {'Name' : 'Metang',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 1,
+        'Hp' : 90,
+        'Power' : False,
+        'Attack1Damage' : 20,
+        'Attack1Effect' : 'None',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 80,
+        'Attack2Effect' : 'None',
+        'Attack2Cost' : 'MMC',
+        'RetreatCost' : 3,
+        'Weakness' : 'R',
+        'Resistance' : 'P'}
+
+card5 = {'Name' : 'Beldum',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 60,
+        'Power' : False,
+        'Attack1Damage' : 20,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'M',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : 'None',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 1,
+        'Weakness' : 'R',
+        'Resistance' : 'P'}
+
+card6 = {'Name' : 'Slowbro',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 1,
+        'Hp' : 110,
+        'Power' : False,
+        'Attack1Damage' : 20,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 50,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'PCC',
+        'RetreatCost' : 3,
+        'Weakness' : 'P',
+        'Resistance' : ''}
+
+card7 = {'Name' : 'Slowpoke',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 70,
+        'Power' : False,
+        'Attack1Damage' : 10,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 60,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'PCC',
+        'RetreatCost' : 3,
+        'Weakness' : 'P',
+        'Resistance' : ''}
+
+card8 = {'Name' : 'Dhelmise',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 120,
+        'Power' : True,
+        'Attack1Damage' : 70,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'PCC',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 2,
+        'Weakness' : 'D',
+        'Resistance' : 'F'}
+
+card9 = {'Name' : 'Oricorio',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 90,
+        'Power' : True,
+        'Attack1Damage' : 30,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'PC',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 1,
+        'Weakness' : 'P',
+        'Resistance' : ''}
+
+card10 = {'Name' : 'Bewear',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 1,
+        'Hp' : 130,
+        'Power' : False,
+        'Attack1Damage' : 60,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'CCC',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 2,
+        'Weakness' : 'F',
+        'Resistance' : ''}
+
+card11 = {'Name' : 'Stufful',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 70,
+        'Power' : False,
+        'Attack1Damage' : 10,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 60,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 2,
+        'Weakness' : 'F',
+        'Resistance' : ''}
+
+card12 = {'Name' : 'Swellow',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 1,
+        'Hp' : 90,
+        'Power' : False,
+        'Attack1Damage' : 20,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 40,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 1,
+        'Weakness' : 'L',
+        'Resistance' : 'F'}
+
+card13 = {'Name' : 'Taillow',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 60,
+        'Power' : False,
+        'Attack1Damage' : 20,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 0,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'None',
+        'RetreatCost' : 1,
+        'Weakness' : 'L',
+        'Resistance' : 'F'}
+
+card14 = {'Name' : 'Castform',
+        'Card_Type' : 'Pokemon',
+        'Stage' : 0,
+        'Hp' : 70,
+        'Power' : False,
+        'Attack1Damage' : 0,
+        'Attack1Effect' : '',
+        'Attack1Cost' : 'C',
+        'Attack2Damage' : 30,
+        'Attack2Effect' : '',
+        'Attack2Cost' : 'CC',
+        'RetreatCost' : 1,
+        'Weakness' : 'F',
+        'Resistance' : ''}
+
+card15 = {'Name' : 'Altar of the Sunne',
+        'Card_Type' : 'Stadium',
+        'Effect' : ''}
+
+card16 = {'Name' : 'Big Malasada',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card17 = {'Name' : 'Energy Retrieval',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card18 = {'Name' : 'Hau',
+        'Card_Type' : 'Supporter',
+        'Effect' : ''}
+
+card19 = {'Name' : 'Nest Ball',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card20 = {'Name' : 'Professor Kukui',
+        'Card_Type' : 'Supporter',
+        'Effect' : ''}
+
+card21 = {'Name' : 'Rescue Stretcher',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card22 = {'Name' : 'Switch',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card23 = {'Name' : 'Timer Ball',
+        'Card_Type' : 'Item',
+        'Effect' : ''}
+
+card24 = {'Name' : 'Metal Energy',
+        'Card_Type' : 'Energy',
+        'Effect' : ''}
+
+card25 = {'Name' : 'Psychic Energy',
+        'Card_Type' : 'Energy',
+        'Effect' : ''}
+
+GameLoop()
+
+
+        
--- GameManager.py
+++ GameManager.py
@@ -40,8 +40,20 @@ class Card():
     Type = ''
     Basic = False
     HP = 0
-    Attack_One = 0
-    Attack_Two = 0
+    Attack_One_Damage = 0
+    Attack_One_Effect = ''
+    Attack_One_Cost = ''
+    Attack_Two_Damage = 0
+    Attack_Two_Effect = ''
+    Attack_Two_Cost = ''
+    
+    Retreat_Cost = 0
+    Pokemon_Type = ''
+    Weakness = ''
+    Resistance = ''
+    #Pre-Evolution = ''
+
+    Effect = ''
 
     def __init__(self, obj):
         self.Name = obj['Name']
@@ -50,12 +62,17 @@ class Card():
             self.Stage = obj['Stage']
             self.Hp = obj['Hp']
             self.Power = obj['Power']
-            self.Attack_One_Damage = obj['Attack1Damage']
+            #self.Attack_One_Damage = obj['Attack1Damage']
             self.Attack_One_Effect = obj['Attack1Effect']
             self.Attack_One_Cost = obj['Attack1Cost']
-            self.Attack_Two_Damage = obj['Attack2Damage']
+            #self.Attack_Two_Damage = obj['Attack2Damage']
             self.Attack_Two_Effect = obj['Attack2Effect']
             self.Attack_Two_Cost = obj['Attack2Cost']
+            self.Retreat_Cost = obj['RetreatCost']
+            self.Weakness = obj['Weakness']
+            self.Resistance = obj['Resistance']
+        if self.Card_Type == 'Item' or self.Card_Type == 'Supporter' or self.Card_Type == 'Stadium' or self.Card_Type == 'Tool':
+            self.Effect = obj['Effect']
     def isBasic(self):
         if self.Card_Type == 'Pokemon':
             return self.Stage == 0

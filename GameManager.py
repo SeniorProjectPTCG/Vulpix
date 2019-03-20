dfee444822f563fddde369e66e3fdd9e9f8785be
Christopher <crisson.christopher@gmail.com>
2019-03-20 10:27:51 -0400

    ready for demo

--- GameManager.py
+++ GameManager.py
@@ -366,7 +366,7 @@ class Gameboard():
             print(self.oppActive[0].Name + " HP: " + str(self.oppActive[0].Hp))
             print(self.playerActive[0].Name + " deals " + str(self.playerActive[0].Attack_One_Damage) + " damage")
             attacks.basicAttack(self.playerActive[0],self.oppActive[0],self.playerActive[0].Attack_One_Damage)
-            print(self.oppActive[0].Hp)
+            print(self.oppActive[0].Name + "HP: " + str(self.oppActive[0].Hp))
             if(self.oppActive[0].Hp <= 0):
                 print(self.oppActive[0].Name + " knocked out!")
                 if len(self.oppBench) > 0:
@@ -384,7 +384,7 @@ class Gameboard():
             print(self.playerActive[0].Name + " HP: " + str(self.playerActive[0].Hp))
             print(self.oppActive[0].Name + " deals " + str(self.oppActive[0].Attack_One_Damage) + " damage")
             attacks.basicAttack(self.oppActive[0],self.playerActive[0],self.oppActive[0].Attack_One_Damage)
-            print(self.playerActive[0].Hp) 
+            print(self.playerActive[0].Name + " HP: " + str(self.playerActive[0].Hp))
             if(self.playerActive[0].Hp <= 0):
                 print(self.playerActive[0].Name + " knocked out!")
                 self.playerDiscard.append(self.playerActive.pop(0))
@@ -508,10 +508,10 @@ class Gameboard():
     def playBasic(self, handIndex, turn):
         ## Plays a basic from hand to bench, space permitting)
         if turn == 'p':
-            if len(self.playerBench) <= 5:
+            if len(self.playerBench) < 5:
                 self.playerBench.append(self.playerHand.pop(handIndex))
         elif turn == 'o':
-            if len(self.oppBench) <= 5:
+            if len(self.oppBench) < 5:
                 self.oppBench.append(self.oppHand.pop(handIndex))
     
     def printHand(self, turn):
